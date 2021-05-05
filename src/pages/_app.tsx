50bf8a1c61228755bcf4b1485e6a67ee347a7c1b
kinakome <ryomatsu0120@gmail.com>
2021-05-05 16:41:28 +0900

    change directory root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
