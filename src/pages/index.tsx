b251a02846dcfa6f14179d48b20c1ded3cda12ec
kinakome <ryomatsu0120@gmail.com>
2021-05-05 16:43:54 +0900

    install sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 
 import Head from "next/head";
 import Image from "next/image";
-import styles from "styles/Home.module.css";
+import styles from "styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
